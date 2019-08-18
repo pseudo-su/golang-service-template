6a095cbe653f6ec7d085f168f12a255af1d6e406
John Stableford <john.stableford@latitudefinancial.com>
2019-08-18 21:41:29 +1000

    feat: graceful shutdown

--- test-suites/smoke/pets_test.go
+++ test-suites/smoke/pets_test.go
@@ -3,34 +3,32 @@ package smoke
 import (
 	"context"
 	"fmt"
-	"testing"
 
 	"github.com/pseudo-su/golang-service-template/pkg"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPets(t *testing.T) {
-	_, apiClient := smokeSuiteSetup()
+func (suite *TestSuite) TestListPets() {
+	t := suite.T()
+	ctx := context.Background()
+	params := pkg.ListPetsParams{}
+	_, err := suite.apiClient.ListPetsWithResponse(ctx, &params)
+	fmt.Println(err)
+	assert.Equal(t, err, nil)
+}
 
-	t.Run("List Pets", func(t *testing.T) {
-		ctx := context.Background()
-		params := pkg.ListPetsParams{}
-		_, err := apiClient.ListPetsWithResponse(ctx, &params)
-		fmt.Println(err)
-		assert.Equal(t, err, nil)
-	})
+func (suite *TestSuite) TestCreatePet() {
+	t := suite.T()
+	ctx := context.Background()
+	_, err := suite.apiClient.CreatePetsWithResponse(ctx)
+	fmt.Println(err)
+	assert.Equal(t, err, nil)
+}
 
-	t.Run("Create Pet", func(t *testing.T) {
-		ctx := context.Background()
-		_, err := apiClient.CreatePetsWithResponse(ctx)
-		fmt.Println(err)
-		assert.Equal(t, err, nil)
-	})
-
-	t.Run("Show Pet By ID", func(t *testing.T) {
-		ctx := context.Background()
-		_, err := apiClient.ShowPetByIdWithResponse(ctx, "2")
-		fmt.Println(err)
-		assert.Equal(t, err, nil)
-	})
+func (suite *TestSuite) TestShowPetById() {
+	t := suite.T()
+	ctx := context.Background()
+	_, err := suite.apiClient.ShowPetByIdWithResponse(ctx, "2")
+	fmt.Println(err)
+	assert.Equal(t, err, nil)
 }
---
+++ test-suites/smoke/smoke_test.go
@@ -0,0 +1,92 @@
+package smoke
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/caarlos0/env"
+	"github.com/pseudo-su/golang-service-template/internal"
+	"github.com/pseudo-su/golang-service-template/internal/config"
+	"github.com/pseudo-su/golang-service-template/pkg"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/suite"
+)
+
+//nolint:go-lint
+type TestSuite struct {
+	suite.Suite
+	server    *config.Server
+	cfg       *TestSuiteConfig
+	apiClient *pkg.ClientWithResponses
+}
+
+func (suite *TestSuite) SetupSuite() {
+	suite.cfg = ParseSuiteConfig()
+	serverBaseURL := buildBaseURL(suite.cfg)
+	suite.apiClient = pkg.NewClientWithResponses(serverBaseURL)
+	suite.server = internal.Bootstrap(suite.cfg)
+
+	if suite.cfg.envValues.UseEmbeddedServer {
+		go func() {
+			err := suite.server.ListenAndServe()
+			if err != nil {
+				log.Printf("Listen and serve: %v", err)
+			}
+		}()
+	}
+}
+
+func (suite *TestSuite) TeardownSuite() {
+	suite.server.ShutdownReq <- true
+}
+
+func TestRunSuite(t *testing.T) {
+	suite.Run(t, new(TestSuite))
+}
+
+func buildBaseURL(cfg *TestSuiteConfig) string {
+	basepath := cfg.ServiceBasepath()
+	return fmt.Sprintf(
+		"%s://%s:%v%s",
+		cfg.envValues.APIScheme,
+		cfg.envValues.APIHost,
+		cfg.envValues.APIPort,
+		basepath,
+	)
+}
+
+type TestSuiteConfig struct {
+	envValues *testSuiteEnv
+}
+
+func (cfg *TestSuiteConfig) Env() string {
+	return cfg.envValues.Env
+}
+
+func (cfg *TestSuiteConfig) ServerPort() int {
+	return cfg.envValues.APIPort
+}
+
+func (cfg *TestSuiteConfig) ServiceBasepath() string {
+	return fmt.Sprintf("/%s/%s", cfg.envValues.ServiceURIName, cfg.envValues.APIVersion)
+}
+
+type testSuiteEnv struct {
+	APIScheme      string `env:"API_SCHEME" envDefault:"http"`
+	APIHost        string `env:"API_HOST" envDefault:"localhost"`
+	APIPort        int    `env:"API_PORT" envDefault:"3000"`
+	ServiceURIName string `env:"SERVICE_URI_NAME" envDefault:"golang-service-template"`
+	APIVersion     string `env:"API_VERSION" envDefault:"v1"`
+	Env            string `env:"ENV" envDefault:"local"`
+	LogLevel       string `env:"LOG_LEVEL" envDefault:"debug"`
+}
+
+func ParseSuiteConfig() *TestSuiteConfig {
+	envValues := &testSuiteEnv{}
+	if err := env.Parse(envValues); err != nil {
+		log.Fatalf("unable to find env var key: %v \n", err)
+	}
+	return &TestSuiteConfig{
+		envValues: envValues,
+	}
+}
