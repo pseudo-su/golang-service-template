1ae896c2051046f50e0b65faa02b61b4cd91d57f
John Stableford <john.stableford@digio.com.au>
2020-03-13 21:03:29 +1100

    chore: remove vendor folder

--- vendor/github.com/pseudo-su/oapi-codegen/pkg/codegen/html/doc.go
+++
@@ -1,3 +0,0 @@
-package templates
-
-//go:generate go run github.com/pseudo-su/templates -t html -s . -o templates.gen.go
--- vendor/github.com/pseudo-su/oapi-codegen/pkg/codegen/templates/doc.go
+++
@@ -1,3 +0,0 @@
-package templates
-
-//go:generate go run github.com/pseudo-su/templates -t text -s . -o templates.gen.go
